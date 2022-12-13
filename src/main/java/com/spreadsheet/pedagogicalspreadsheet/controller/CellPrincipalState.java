8d2215f7172f0b16fb75d5a3022c4a7a5b4df66f
Flandre Corenitn <flandrecorentin@gmail.com>
2022-12-13 13:04:24 +0100

    - Affichage de la navigation entre chaques PrincipalState de la barre de navigation

--- src/main/java/com/spreadsheet/pedagogicalspreadsheet/controller/CellPrincipalState.java
+++ src/main/java/com/spreadsheet/pedagogicalspreadsheet/controller/CellPrincipalState.java
@@ -1,4 +1,57 @@
 package com.spreadsheet.pedagogicalspreadsheet.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CellPrincipalState implements State{
+    private static final Logger logger = LoggerFactory.getLogger(CellPrincipalState.class);
+
+    @Override
+    public void displayFilePrincipalState(Controller c){
+        logger.trace("start displayFilePrincipalState(Controller c)");
+        // Style for navbar
+        c.changeActivedNavBarButton(c.getCellNavBarButton());
+        c.changeActivedNavBarButton(c.getFileNavBarButton());
+        // display diagramNavToolsBar
+
+        Controller.setCurrentState(c.filePrincipalState);
+        logger.trace("end start displayFilePrincipalState(Controller c)");
+    }
+
+
+    @Override
+    public void displayDiagramPrincipalState(Controller c){
+        logger.trace("start displayDiagramPrincipalState(Controller c)");
+        // Style for navbar
+        c.changeActivedNavBarButton(c.getCellNavBarButton());
+        c.changeActivedNavBarButton(c.getDiagramNavBarButton());
+        // display diagramNavToolsBar
+
+        Controller.setCurrentState(c.diagramPrincipalState);
+        logger.trace("end start displayDiagramPrincipalState(Controller c)");
+    }
+
+    @Override
+    public void displayGamePrincipalState(Controller c){
+        logger.trace("start displayGamePrincipalState(Controller c)");
+        // Style for navbar
+        c.changeActivedNavBarButton(c.getCellNavBarButton());
+        c.changeActivedNavBarButton(c.getGameNavBarButton());
+        // display gameNavToolsBar
+
+        Controller.setCurrentState(c.gamePrincipalState);
+        logger.trace("end start displayGamePrincipalState(Controller c)");
+    }
+
+    @Override
+    public void displayHelpPrincipalState(Controller c){
+        logger.trace("start displayHelpPrincipalState(Controller c)");
+        // Style for navbar
+        c.changeActivedNavBarButton(c.getCellNavBarButton());
+        c.changeActivedNavBarButton(c.getHelpNavBarButton());
+        // display gameNavToolsBar
+
+        Controller.setCurrentState(c.helpPrincipalState);
+        logger.trace("end start displayHelpPrincipalState(Controller c)");
+    }
 }
--- src/main/java/com/spreadsheet/pedagogicalspreadsheet/controller/GamePrincipalState.java
+++ src/main/java/com/spreadsheet/pedagogicalspreadsheet/controller/GamePrincipalState.java
@@ -1,4 +1,57 @@
 package com.spreadsheet.pedagogicalspreadsheet.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class GamePrincipalState implements State{
+
+    private static final Logger logger = LoggerFactory.getLogger(GamePrincipalState.class);
+
+    @Override
+    public void displayFilePrincipalState(Controller c){
+        logger.trace("start displayFilePrincipalState(Controller c)");
+        // Style for navbar
+        c.changeActivedNavBarButton(c.getGameNavBarButton());
+        c.changeActivedNavBarButton(c.getFileNavBarButton());
+        // display diagramNavToolsBar
+
+        Controller.setCurrentState(c.filePrincipalState);
+        logger.trace("end start displayFilePrincipalState(Controller c)");
+    }
+
+    @Override
+    public void displayCellPrincipalState(Controller c){
+        logger.trace("start displayCellPrincipalState(Controller c)");
+        // Style for navbar
+        c.changeActivedNavBarButton(c.getGameNavBarButton());
+        c.changeActivedNavBarButton(c.getCellNavBarButton());
+        // display cellNavToolsBar
+
+        Controller.setCurrentState(c.cellPrincipalState);
+        logger.trace("end start displayCellPrincipalState(Controller c)");
+    }
+
+    @Override
+    public void displayDiagramPrincipalState(Controller c){
+        logger.trace("start displayDiagramPrincipalState(Controller c)");
+        // Style for navbar
+        c.changeActivedNavBarButton(c.getGameNavBarButton());
+        c.changeActivedNavBarButton(c.getDiagramNavBarButton());
+        // display diagramNavToolsBar
+
+        Controller.setCurrentState(c.diagramPrincipalState);
+        logger.trace("end start displayDiagramPrincipalState(Controller c)");
+    }
+
+    @Override
+    public void displayHelpPrincipalState(Controller c){
+        logger.trace("start displayHelpPrincipalState(Controller c)");
+        // Style for navbar
+        c.changeActivedNavBarButton(c.getGameNavBarButton());
+        c.changeActivedNavBarButton(c.getHelpNavBarButton());
+        // display gameNavToolsBar
+
+        Controller.setCurrentState(c.helpPrincipalState);
+        logger.trace("end start displayHelpPrincipalState(Controller c)");
+    }
 }
