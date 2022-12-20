1844085364e59ec2ad919a3759d7270d078add14
Flandre Corenitn <flandrecorentin@gmail.com>
2022-12-20 01:39:21 +0100

    - /!\ gros commit and push /!\ - oups j'avais oublié de commit la class MyControlSkin

---
+++ src/main/java/com/spreadsheet/pedagogicalspreadsheet/view/MyControlSkin.java
@@ -0,0 +1,12 @@
+package com.spreadsheet.pedagogicalspreadsheet.view;
+
+import javafx.scene.control.SkinBase;
+
+public class MyControlSkin extends SkinBase<CellView> {
+
+    public MyControlSkin(CellView control) {
+        super(control);
+        // Initialize skin elements and layout here
+    }
+    // Override other Skin methods to customize the appearance of the control
+}
